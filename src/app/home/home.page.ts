5088149715ad3a72f72bd798192cde6d69eec615
muhammed rifkhan <rifkhanmuhammed17@gmail.com>
2022-06-21 22:49:19 +0530

    front page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,34 @@ export class HomePage {
 
   constructor() {}
 
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  todoList = [
+    {
+    itemName:'Coding',
+    itemDueDate:'13-12-22',
+    itemPriority:'high',
+    itemCategory:'work'
+  },
+  {
+    itemName:'Design',
+    itemDueDate:'23-10-22',
+    itemPriority:'low',
+    itemCategory:'Personal'
+  },
+  {
+    itemName:'Shopping',
+    itemDueDate:'13-10-22',
+    itemPriority:'middle',
+    itemCategory:'work'
+  },
+  {
+    itemName:'Workout',
+    itemDueDate:'17-03-22',
+    itemPriority:'high',
+    itemCategory:'work'
+  }
+];
+
+today: number = Date.now();
+
 }
